7b53ccbc1b5c57b8dc3df134f91926a0d0e99ced
Shantanu Goel <shantanu.goel@gmail.com>
2015-08-20 01:27:35 +0530

    Make sizes as foreign keys to avoid people putting in wrong values

--- tee/models.py
+++ tee/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 
+class Size(models.Model):
+    size = models.CharField(max_length=3, null=True)
+
+    def __str__(self):
+        return self.size
+
+
 class Tee(models.Model):
     qid = models.CharField(max_length=255, null=True)
-    havesize = models.CharField(max_length=3, null=True)
-    wantsize = models.CharField(max_length=3, null=True)
+    havesize = models.ForeignKey(Size, related_name='havesize', null=True)
+    wantsize = models.ForeignKey(Size, related_name='wantsize', null=True)
     havecolor = models.CharField(max_length=10, null=True)
     traded = models.BooleanField(default=False)
 
